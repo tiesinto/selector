80e3234e3add4c4e10f929946f203b97aef416f9
tiesinto <141479594+tiesinto@users.noreply.github.com>
2026-01-15 20:12:16 -0500

    Update and rename config.h to config.def.h

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Noto Sans Mono:size=8"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#34345c", "#e6cbc0" },
-	[SchemeSel] = { "#ff0000", "#d9af9e" },
-	[SchemePrompt] = { "#005577", "#eeeeee" },
+	/*                   fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemePrompt] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
